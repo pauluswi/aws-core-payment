1d899222344fe3931b3ed06443d225b39d671609
pauluswi <pswied@gmail.com>
2026-09-11 08:40:51 +0700

    Initiate Java Maven Project

---
+++ src/main/java/com/pswied/loan/awscorepayment/AwsCorePaymentApplication.java
@@ -0,0 +1,12 @@
+package com.pswied.loan.awscorepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCorePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsCorePaymentApplication.class, args);
+    }
+}
---
+++ src/test/java/com/pswied/loan/awscorepayment/AwsCorePaymentApplicationTests.java
@@ -0,0 +1,12 @@
+package com.pswied.loan.awscorepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCorePaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
